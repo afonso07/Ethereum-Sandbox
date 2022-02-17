9ca4e689fce4fadf5ddd045b0c21bf9600cfcebc
afonso.lopocarvalho@gmail.com <afonso.lopocarvalho@gmail.com>
2022-02-17 19:04:10 +0000

    first commit

---
+++ eth-todo-list/contracts/TodoList.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.11; // Source files can (and should) be annotated with a version pragma to reject compilation with future compiler versions that might introduce incompatible changes. 
+
+
+contract TodoList{
+    /**
+    Setting the variable to public allows the variiable to be accessed
+    outside of the contract. Solidity automatically creates a taskCount()
+    function to access this value
+     */ 
+    uint public taskCount = 0;
+}
