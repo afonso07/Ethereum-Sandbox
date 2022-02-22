dd1c9408cadf6aa889926578db0419a6f1dc5485
afonso.lopocarvalho@gmail.com <afonso.lopocarvalho@gmail.com>
2022-02-22 19:21:09 +0000

    added styling

---
+++ eth-todo-list-next-fn/components/TodoList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/TodoList.module.css";
+
+const TodoList = (): JSX.Element => {
+  return <div className={styles.container}>This is a todo list</div>;
+};
+
+export default TodoList;
--- eth-todo-list-next-fn/pages/index.tsx
+++ eth-todo-list-next-fn/pages/index.tsx
@@ -1,14 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import TodoList from "../components/TodoList";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-    
+      <TodoList />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
