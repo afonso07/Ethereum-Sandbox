c277660bc87f24ac0b2b89c573079399aa74e4fa
afonso.lopocarvalho@gmail.com <afonso.lopocarvalho@gmail.com>
2022-02-27 17:28:50 +0000

    adding extra function

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.12; // Source files can (and should) be annotated with a version pragma to reject compilation with future compiler versions that might introduce incompatible changes. 
+pragma solidity 0.8.12; // Source files can (and should) be annotated with a version pragma to reject compilation with future compiler versions that might introduce incompatible changes.
 
-
-contract TodoList{
-
+contract TodoList {
     // This gets run once when the contract is deployed, creating a defaut task
-    constructor(){
+    constructor() {
         createTask("Default task");
     }
 
@@ -13,21 +11,24 @@ contract TodoList{
     Setting the variable to public allows the variiable to be accessed
     outside of the contract. Solidity automatically creates a taskCount()
     function to access this value
-     */ 
-    uint public taskCount = 0;
+     */
+    uint256 public taskCount = 0;
 
-    struct Task{
-        uint id;
+    struct Task {
+        uint256 id;
         string content;
         bool completed;
     }
 
+    event TaskCreated(uint256 id, string content, bool completed);
+
     // This is a mapping of task ID to Task
-    mapping(uint => Task) public tasks;
+    mapping(uint256 => Task) public tasks;
 
     //A function that creates and adds a task to the mappings
-    function createTask(string memory _content) public{
-        taskCount ++;
+    function createTask(string memory _content) public {
+        taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }
