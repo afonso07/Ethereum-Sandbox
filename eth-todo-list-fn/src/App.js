b82de4f1fdfd7db97c88be051c548d5fc563149c
afonso.lopocarvalho@gmail.com <afonso.lopocarvalho@gmail.com>
2022-02-27 22:09:07 +0000

    backup save

--- eth-todo-list-fn/src/App.js
+++ eth-todo-list-fn/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import TodoList from "./components/TodoList";
 import Toolbar from "./components/Utils";
 import "./css/Utils.css";
+
 function App() {
   return (
     <div className="App">
       <Toolbar />
+      <TodoList />
     </div>
   );
 }
---
+++ eth-todo-list-fn/src/components/TodoList.js
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import Web3 from "web3";
+
+const TodoList = () => {
+  console.log(Web3.givenProvider);
+  useEffect(() => {}, []);
+
+  return <div></div>;
+};
+
+export default TodoList;
